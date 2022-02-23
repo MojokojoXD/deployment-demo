c1b7a45efe13c3ae6f27807e9b8bf89f3d641aa5
Kojo Boakye-Yiadom <kwadwoneer@gmail.com>
2022-02-23 13:32:16 -0700

    modified Home.html

--- server/index.js
+++ server/index.js
@@ -8,7 +8,6 @@ const port = process.env.PORT || 4005;
 
 app.get('/', (req,res) => {
     res.sendFile(path.join(__dirname, '../Home.html'));
-    res.sendFile(path.join(__dirname,'../pecedis-style.css'));
 })
 
 app.listen(port, () => {
