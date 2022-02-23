8128113036f70b5d3286489d82ee176af90223f4
Kojo Boakye-Yiadom <kwadwoneer@gmail.com>
2022-02-23 14:37:54 -0700

    added static statement to server(index.js)

--- server/index.js
+++ server/index.js
@@ -9,6 +9,7 @@ const port = process.env.PORT || 4005;
 app.get('/', (req,res) => {
     res.sendFile(path.join(__dirname, '../public','Home.html'));
 })
+app.use(express.static(path.join(__dirname,'../public')))
 
 app.listen(port, () => {
     console.log(`Server running on ${port}`)
