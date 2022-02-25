d0d144d7a864630f89a600531dd104fa06742032
Kojo Boakye-Yiadom <kwadwoneer@gmail.com>
2022-02-25 16:07:16 -0700

    trying to get rid of hover effect on mobile

---
+++ public/Home.js
@@ -0,0 +1,6 @@
+const touchsupport = ('ontouchstart' in window) || (navigator.maxTouchPoints > 0) || (navigator.msMaxTouchPoints > 0)
+
+
+if (!touchsupport){ 
+    document.documentElement.className += " non-touch"
+}
