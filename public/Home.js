3b3ddf05fc5fcfb4687b37d16b31a90f88f43c52
Kojo Boakye-Yiadom <kwadwoneer@gmail.com>
2022-02-26 17:37:16 -0700

    managed to make a function drop-down in desktop version

--- public/Home.js
+++
@@ -1,6 +0,0 @@
-const touchsupport = ('ontouchstart' in window) || (navigator.maxTouchPoints > 0) || (navigator.msMaxTouchPoints > 0)
-
-
-if (!touchsupport){ 
-    document.documentElement.className += 'non-touch';
-}
---
+++ public/loans.js
@@ -0,0 +1,24 @@
+const menuHover = document.querySelectorAll('.nav-menu');
+const drop_down_div = document.querySelectorAll('.drop-down');
+
+for(let i = 0; i < menuHover.length; i++){
+    menuHover[i].addEventListener('mouseover', () => {
+        menuHover[i].classList.add('hover')
+        drop_down_div[i].style.display = 'block'
+    
+        drop_down_div[i].addEventListener('mouseover', () => {
+            menuHover[i].classList.add('hover')
+            drop_down_div[i].style.display = 'block'
+        })
+
+        drop_down_div[i].addEventListener('mouseout', () => {
+            menuHover[i].classList.remove('hover');
+            drop_down_div[i].style.display = 'none'
+        })
+    })
+
+    menuHover[i].addEventListener('mouseout', () => {
+        menuHover[i].classList.remove('hover');
+        drop_down_div[i].style.display = 'none'
+    })
+}
