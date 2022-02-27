3734c2c80fe3a4906d681215b295e11082678ee3
Kojo Boakye-Yiadom <kwadwoneer@gmail.com>
2022-02-26 23:00:00 -0700

    added side bar to mobile version

--- public/loans.js
+++ public/loans.js
@@ -1,5 +1,7 @@
 const menuHover = document.querySelectorAll('.nav-menu');
 const drop_down_div = document.querySelectorAll('.drop-down');
+const mobile_menu = document.querySelector('#phone-menu')
+const side_bar = document.querySelector('#mobile-drop-down')
 
 for(let i = 0; i < menuHover.length; i++){
     menuHover[i].addEventListener('mouseover', () => {
@@ -22,3 +24,13 @@ for(let i = 0; i < menuHover.length; i++){
         drop_down_div[i].style.display = 'none'
     })
 }
+
+
+mobile_menu.addEventListener('click', () => {
+    if(side_bar.style.display === 'block'){
+        side_bar.style.display = 'none'
+    }
+    else{
+        side_bar.style.display = 'block'
+    }
+})
